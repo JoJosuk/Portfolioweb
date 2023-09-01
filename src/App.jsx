0f5bad870abac922736d73e48824a940ef2467b0
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-09-02 00:37:49 +0530

    feat: added terminal.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
 import "./App.css";
 // eslint-disable-next-line no-unused-vars
 import Terminal from "./components/terminal";
-import Typewriter from "typewriter-effect";
+// import Typewriter from "typewriter-effect";
 
 function App() {
   return (
-    // <Terminal />
-    <>
-      <div className="noise"></div>
-      <div className="overlay">
-        <div className="terminal">
-          <h1>
-            Hey I am
-            <span className="errorcode">
-              <Typewriter
-                options={{
-                  strings: ["Joel J George"],
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
-            </span>
-          </h1>
-          <div className="input">
-            <input type="text" name="" id="" />
-          </div>{" "}
-          <p className="output">
-            Goal-oriented software developer engineer with a passion for
-            creating desktop apps and exploring web technologies
-          </p>
-          <p className="output">
-            Please try to <a href="#1">go back</a> or{" "}
-            <a href="#2">return to the homepage</a>.
-          </p>
-          <p className="output">Good luck.</p>
-        </div>
-      </div>
-    </>
+    <Terminal />
+    // <>
+    //   <div className="noise"></div>
+    //   <div className="overlay">
+    //     <div className="terminal">
+    //       <h1>
+    //         Hey I am
+    //         <span className="errorcode">
+    //           <Typewriter
+    //             options={{
+    //               strings: ["Joel J George"],
+    //               autoStart: true,
+    //               loop: true,
+    //             }}
+    //           />
+    //         </span>
+    //       </h1>
+    //       <div className="input">
+    //         <input type="text" name="" id="" />
+    //       </div>{" "}
+    //       <p className="output">
+    //         Goal-oriented software developer engineer with a passion for
+    //         creating desktop apps and exploring web technologies
+    //       </p>
+    //       <p className="output">
+    //         Please try to <a href="#1">go back</a> or{" "}
+    //         <a href="#2">return to the homepage</a>.
+    //       </p>
+    //       <p className="output">Good luck.</p>
+    //     </div>
+    //   </div>
+    // </>
   );
 }
 
