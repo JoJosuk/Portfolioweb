00b2e48dab23d17731b6008ebc4fe6495a5cbd8c
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-09-03 02:40:04 +0530

    feat: added the filemanager shell

--- src/components/FIleManager.jsx
+++ src/components/FIleManager.jsx
@@ -1,7 +1,12 @@
 export default function FileManager() {
   return (
     <>
-      <div className="bg-hero-pattern h-screen w-screen bg-cover"></div>
+      <div className="bg-hero-pattern h-screen w-screen bg-cover flex flex-col items-center justify-center ">
+        <div className="w-3/5 h-4/5 rounded-2xl  flex">
+          <div className="w-1/4 bg-slate-200 rounded-l-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 "></div>
+          <div className=" w-3/4 bg-white rounded-r-2xl"></div>
+        </div>
+      </div>
     </>
   );
 }
