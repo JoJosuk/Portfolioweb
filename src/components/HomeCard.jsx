363bc7ed51693f915a686bc88ecafc4f84d270d7
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-09-03 13:21:56 +0530

    feat: added the headings

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,7 +1,16 @@
 export default function HomeCard() {
   return (
     <>
-      <div className="flex items-center justify-center h-full"></div>
+      <div className="flex items-center justify-center h-full">
+        <div className="w-4/5 p-4 bg-cover h-4/5 rounded-2xl bg-card-img">
+          <p className="p-2 font-sans text-lg font-bold">
+            Software Developer Engineer
+          </p>
+          <h1 className="p-2 font-sans text-5xl text-white">
+            Hey, I&rsquo;m Joel J George
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
