4a5213a4d6a6e2d2b38dec2a3fbcd12236e0935a
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-09-05 01:52:58 +0530

    feat : added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // eslint-disable-next-line no-unused-vars
-import FileManager from "./components/FIleManager";
+// import FileManager from "./components/FIleManager";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Connect from "./components/Connect";
+import Home from "./components/Home";
 
 // import About from "./components/About";
 // import Projects from "./components/Projects";
@@ -17,7 +18,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<FileManager />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
         <Route path="/connect" element={<Connect />}></Route>
